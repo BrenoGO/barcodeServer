eabf62604f4d740f3b8acf2e4f31d8ab7b7aaa88
Breno Oliveira <breno.oliveira@bicyclehealth.com>
2022-01-24 09:13:02 -0300

    minor changes

--- src/server.js
+++ src/server.js
@@ -5,20 +5,22 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/sendData', (req, res) => {
-  console.log('in sendData');
+app.post('/save', (req, res) => {
+  // console.log('in sendData');
   try{
-    const { estoque } = req.body;
+    const { estoque, user, project } = req.body;
 
     const timestamp = Date.now();
-    const fileName = `${timestamp}.txt`;
-    const finalFileName = 'estoque.txt';
+    const fileName = `${user}-${timestamp}.txt`;
+    const finalFileName = `${project}.txt`;
 
     fs.writeFileSync(path.resolve(__dirname, 'files', fileName), estoque);
     fs.appendFileSync(path.resolve(__dirname, 'files', finalFileName), estoque);
     return res.status(200).json({success: 'Data was saved!'})
   }catch(err) {
-    return res.status(400).json({err});
+    console.log('error saving data..');
+    console.log(err);
+    return res.status(500).json({err: err.message});
   }
 })
 
